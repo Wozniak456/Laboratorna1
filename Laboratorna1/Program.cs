4ac3c8738632c1c9ba180adab44484ab6b8a459b
Peter Woznyak <woznyak@hotmail.com>
2020-10-18 18:21:24 +0300

    Add project files.

---
+++ Laboratorna1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Laboratorna1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int b, q, n; double S;   //описуємо змінні
+
+            Console.WriteLine("vvedit b: "); //перший член прогресії
+            int.TryParse(Console.ReadLine(), out b); // конвертація з рядкового типу в чисельний і присвоєння змінній
+            Console.WriteLine("vvedit q: ");  //знаменник прогресії
+            int.TryParse(Console.ReadLine(), out q);
+            Console.WriteLine("vvedit n: ");  //кількість членів прогресії
+            int.TryParse(Console.ReadLine(), out n);
+
+            S = (b * (Math.Pow(q, n) - 1)) / (q - 1);  // формула для обрахування суми 
+
+            Console.WriteLine("Suma = {0}", S);  //виведення на консоль
+
+            Console.ReadKey();  // затримка екрана
+        }
+    }
+}
